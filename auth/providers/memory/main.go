2db32544eff4a6c8970d1edf7b78193855eab8f6
Alexandre Pires <c.alexandre.pires@gmail.com>
2025-04-20 23:28:57 +0200

    refactor: Correct struct name from MemoryAuthProvide to MemoryAuthProvider for consistency

--- auth/providers/memory/main.go
+++ auth/providers/memory/main.go
@@ -9,18 +9,18 @@ import (
 	providerTypes "github.com/a13labs/a13core/auth/providers/types"
 )
 
-type MemoryAuthProvide struct {
+type MemoryAuthProvider struct {
 	providerTypes.AuthProvider
 	users map[string]*providerTypes.User
 }
 
 func FromConfig(config json.RawMessage) providerTypes.AuthProvider {
-	return &MemoryAuthProvide{
+	return &MemoryAuthProvider{
 		users: make(map[string]*providerTypes.User),
 	}
 }
 
-func (a *MemoryAuthProvide) AuthenticateUser(username, password string) *providerTypes.UserView {
+func (a *MemoryAuthProvider) AuthenticateUser(username, password string) *providerTypes.UserView {
 	if user, ok := a.users[username]; ok {
 		if internal.VerifyPassword(user.Hash, password) {
 			return &providerTypes.UserView{
@@ -32,7 +32,7 @@ func (a *MemoryAuthProvide) AuthenticateUser(username, password string) *provide
 	return nil
 }
 
-func (a *MemoryAuthProvide) AuthenticateWithAppPassword(username, password string) *providerTypes.UserView {
+func (a *MemoryAuthProvider) AuthenticateWithAppPassword(username, password string) *providerTypes.UserView {
 	if user, ok := a.users[username]; ok {
 		for _, appPassword := range user.AppPasswords {
 			if internal.VerifyPassword(appPassword.Hash, password) && !appPassword.Revoked {
@@ -57,7 +57,7 @@ func (a *MemoryAuthProvide) AuthenticateWithAppPassword(username, password strin
 	return nil
 }
 
-func (a *MemoryAuthProvide) AddUser(username, password, role string) error {
+func (a *MemoryAuthProvider) AddUser(username, password, role string) error {
 
 	if _, ok := a.users[username]; ok {
 		return fmt.Errorf("user already exists")
@@ -78,7 +78,7 @@ func (a *MemoryAuthProvide) AddUser(username, password, role string) error {
 	return nil
 }
 
-func (a *MemoryAuthProvide) RemoveUser(username string) error {
+func (a *MemoryAuthProvider) RemoveUser(username string) error {
 
 	if _, ok := a.users[username]; !ok {
 		return fmt.Errorf("user does not exist")
@@ -88,7 +88,7 @@ func (a *MemoryAuthProvide) RemoveUser(username string) error {
 	return nil
 }
 
-func (a *MemoryAuthProvide) GetUsers() ([]providerTypes.UserView, error) {
+func (a *MemoryAuthProvider) GetUsers() ([]providerTypes.UserView, error) {
 	users := make([]providerTypes.UserView, 0, len(a.users))
 	i := 0
 	for user := range a.users {
@@ -111,7 +111,7 @@ func (a *MemoryAuthProvide) GetUsers() ([]providerTypes.UserView, error) {
 	return users, nil
 }
 
-func (a *MemoryAuthProvide) ChangePassword(username, password string) error {
+func (a *MemoryAuthProvider) ChangePassword(username, password string) error {
 	if _, ok := a.users[username]; !ok {
 		return fmt.Errorf("user does not exist")
 	}
@@ -123,23 +123,23 @@ func (a *MemoryAuthProvide) ChangePassword(username, password string) error {
 	return nil
 }
 
-func (a *MemoryAuthProvide) DropUsers() error {
+func (a *MemoryAuthProvider) DropUsers() error {
 	a.users = make(map[string]*providerTypes.User)
 	return nil
 }
 
-func (a *MemoryAuthProvide) LoadUsers() error {
+func (a *MemoryAuthProvider) LoadUsers() error {
 	return nil
 }
 
-func (a *MemoryAuthProvide) GetRole(username string) (string, error) {
+func (a *MemoryAuthProvider) GetRole(username string) (string, error) {
 	if user, ok := a.users[username]; ok {
 		return user.Role, nil
 	}
 	return "", fmt.Errorf("user does not exist")
 }
 
-func (a *MemoryAuthProvide) GetUser(username string) (providerTypes.UserView, error) {
+func (a *MemoryAuthProvider) GetUser(username string) (providerTypes.UserView, error) {
 	if user, ok := a.users[username]; ok {
 		userView := providerTypes.UserView{
 			Username:     user.Username,
@@ -160,7 +160,7 @@ func (a *MemoryAuthProvide) GetUser(username string) (providerTypes.UserView, er
 	return providerTypes.UserView{}, fmt.Errorf("user does not exist")
 }
 
-func (a *MemoryAuthProvide) SetRole(username, role string) error {
+func (a *MemoryAuthProvider) SetRole(username, role string) error {
 	if user, ok := a.users[username]; ok {
 		user.Role = role
 		return nil
@@ -168,7 +168,7 @@ func (a *MemoryAuthProvide) SetRole(username, role string) error {
 	return fmt.Errorf("user does not exist")
 }
 
-func (a *MemoryAuthProvide) AddAppPassword(username, password, role string, expire int) (string, error) {
+func (a *MemoryAuthProvider) AddAppPassword(username, password, role string, expire int) (string, error) {
 	if user, ok := a.users[username]; ok {
 
 		hash, err := internal.HashPassword(password)
@@ -190,7 +190,7 @@ func (a *MemoryAuthProvide) AddAppPassword(username, password, role string, expi
 	return "", fmt.Errorf("user does not exist")
 }
 
-func (a *MemoryAuthProvide) RevokeAppPassword(username, id string) error {
+func (a *MemoryAuthProvider) RevokeAppPassword(username, id string) error {
 	if user, ok := a.users[username]; ok {
 		for i, appPassword := range user.AppPasswords {
 			if appPassword.ID == id {
@@ -202,7 +202,7 @@ func (a *MemoryAuthProvide) RevokeAppPassword(username, id string) error {
 	return fmt.Errorf("user or app password does not exist")
 }
 
-func (a *MemoryAuthProvide) GetAppPasswords(username string) ([]providerTypes.AppPasswordView, error) {
+func (a *MemoryAuthProvider) GetAppPasswords(username string) ([]providerTypes.AppPasswordView, error) {
 	if user, ok := a.users[username]; ok {
 		appPasswords := make([]providerTypes.AppPasswordView, 0, len(user.AppPasswords))
 		for _, appPassword := range user.AppPasswords {
@@ -219,7 +219,7 @@ func (a *MemoryAuthProvide) GetAppPasswords(username string) ([]providerTypes.Ap
 	return nil, fmt.Errorf("user does not exist")
 }
 
-func (a *MemoryAuthProvide) CleanUpRevokedExpiredAppPasswords() error {
+func (a *MemoryAuthProvider) CleanUpRevokedExpiredAppPasswords() error {
 	for _, user := range a.users {
 		for i := len(user.AppPasswords) - 1; i >= 0; i-- {
 			appPassword := user.AppPasswords[i]
